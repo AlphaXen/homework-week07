4014dc47650d365124ce941dec63bd7535b3c0e2
AlphaXen <kimminjae771@naver.com>
2025-10-20 00:11:09 +0900

    Updated Changes

--- lib/week07/calendar_scheduler/database/drift_database.dart
+++ lib/week07/calendar_scheduler/database/drift_database.dart
@@ -8,6 +8,10 @@ part 'drift_database.g.dart'; // part 파일 지정
   // 사용할 테이블 등록
   tables: [Schedules],
 )
-class LocalDatabase extends _$LocalDatabase {}
+class LocalDatabase extends _$LocalDatabase {
+  Stream<List<Schedule>> watchSchedules(DateTime date) =>
+      // 데이터를 초기화하고 변화 감지
+      (select(schedules)..where((tbl) => tbl.date.equals(date))).watch();
+}
 
 // Code Generation으로 생성할 클래스 상속
