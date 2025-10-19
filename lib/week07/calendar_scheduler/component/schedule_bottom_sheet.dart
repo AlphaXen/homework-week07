d5f111119cf1165519c69036991beb27e500f1fa
AlphaXen <kimminjae771@naver.com>
2025-10-20 00:34:09 +0900

    Updated Changes

--- lib/week07/calendar_scheduler/component/schedule_bottom_sheet.dart
+++ lib/week07/calendar_scheduler/component/schedule_bottom_sheet.dart
@@ -75,7 +75,7 @@ class _ScheduleBottomState extends State<ScheduleBottomSheet> {
                     label: '내용',
                     isTime: false,
                     onSaved: (String? val) {
-                      // 저장이 실행되면 startTime 변수에 텍스트 필드값 저장
+                      // 저장이 실행되면 content 변수에 텍스트 필드값 저장
                       content = val;
                     },
                     validator: contentValidator,
