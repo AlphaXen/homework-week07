335e34afab43a16e707f7bba40cb82b6c0e989d5
AlphaXen <kimminjae771@naver.com>
2025-10-20 00:29:09 +0900

    Updated Changes

--- lib/week07/calendar_scheduler/component/schedule_bottom_sheet.dart
+++ lib/week07/calendar_scheduler/component/schedule_bottom_sheet.dart
@@ -23,64 +23,63 @@ class _ScheduleBottomState extends State<ScheduleBottomSheet> {
 
     return Form(
       key: formKey,
-      child: ,
-    )
-    return SafeArea(
-      child: Container(
-        height: MediaQuery.of(context).size.height / 2 + bottomInset,
-        color: Colors.white,
-        child: Padding(
-          padding: EdgeInsets.only(
-            left: 8,
-            right: 8,
-            top: 8,
-            bottom: bottomInset,
-          ), // 패딩에 키보드 높이 추가해서 위젯 전반적으로 위로 올려주기
-          child: Column(
-            // 시간 관련 텍스트 필드와 내용 관련 텍스트 필드 세로로 배치
-            children: [
-              Row(
-                // 시작 시간, 종료 시간 가로로 배치
-                children: [
-                  Expanded(
-                    child: CustomTextField(
-                      // 시작 시간 입력 필드
-                      label: '시작 시간',
-                      isTime: true,
+      child: SafeArea(
+        child: Container(
+          height: MediaQuery.of(context).size.height / 2 + bottomInset,
+          color: Colors.white,
+          child: Padding(
+            padding: EdgeInsets.only(
+              left: 8,
+              right: 8,
+              top: 8,
+              bottom: bottomInset,
+            ), // 패딩에 키보드 높이 추가해서 위젯 전반적으로 위로 올려주기
+            child: Column(
+              // 시간 관련 텍스트 필드와 내용 관련 텍스트 필드 세로로 배치
+              children: [
+                Row(
+                  // 시작 시간, 종료 시간 가로로 배치
+                  children: [
+                    Expanded(
+                      child: CustomTextField(
+                        // 시작 시간 입력 필드
+                        label: '시작 시간',
+                        isTime: true,
+                      ),
                     ),
-                  ),
-                  const SizedBox(width: 16.0),
-                  Expanded(
-                    child: CustomTextField(
-                      // 종료 시간 입력 필드
-                      label: '종료 시간',
-                      isTime: true,
+                    const SizedBox(width: 16.0),
+                    Expanded(
+                      child: CustomTextField(
+                        // 종료 시간 입력 필드
+                        label: '종료 시간',
+                        isTime: true,
+                      ),
                     ),
-                  ),
-                ],
-              ),
-              SizedBox(height: 8.0),
-              Expanded(
-                child: CustomTextField(
-                  // 내용 입력 필드
-                  label: '내용',
-                  isTime: false,
+                  ],
                 ),
-              ),
-              SizedBox(
-                width: double.infinity,
-                child: ElevatedButton(
-                  // [저장] 버튼
-                  onPressed: onSavePressed,
-                  // [저장] 버튼
-                  style: ElevatedButton.styleFrom(
-                    foregroundColor: Colors.white,
-                    backgroundColor: PRIMARY_COLOR,
+                SizedBox(height: 8.0),
+                Expanded(
+                  child: CustomTextField(
+                    // 내용 입력 필드
+                    label: '내용',
+                    isTime: false,
                   ),
-                  child: Text('저장'),
                 ),
-              ),
-            ],
+                SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    // [저장] 버튼
+                    onPressed: onSavePressed,
+                    // [저장] 버튼
+                    style: ElevatedButton.styleFrom(
+                      foregroundColor: Colors.white,
+                      backgroundColor: PRIMARY_COLOR,
+                    ),
+                    child: Text('저장'),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
