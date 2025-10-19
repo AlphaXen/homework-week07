c14fb5f7c0bbf7651767cf5a96b1ec99772c82fd
AlphaXen <kimminjae771@naver.com>
2025-10-20 01:02:40 +0900

    Updated Changes

--- lib/week07/calendar_scheduler/screen/home_screen.dart
+++ lib/week07/calendar_scheduler/screen/home_screen.dart
@@ -80,20 +80,19 @@ class _HomeScreen extends State<HomeScreen> {
                           GetIt.I<LocalDatabase>().removeSchedule(schedule.id);
                         },
                         child: Padding(
-                        // 좌우로 패딩을 추가해서 UI 개선
-                        padding: const EdgeInsets.only(
-                          bottom: 8.0,
-                          left: 8.0,
-                          right: 8.0,
+                          // 좌우로 패딩을 추가해서 UI 개선
+                          padding: const EdgeInsets.only(
+                            bottom: 8.0,
+                            left: 8.0,
+                            right: 8.0,
+                          ),
+                          child: ScheduleCard(
+                            startTime: schedule.startTime,
+                            endTime: schedule.endTime,
+                            content: schedule.content,
+                          ),
                         ),
-                        child: ScheduleCard(
-                          startTime: schedule.startTime,
-                          endTime: schedule.endTime,
-                          content: schedule.content,
-                        ),
-                      );,
-                      )
-    
+                      );
                     },
                   );
                 },
